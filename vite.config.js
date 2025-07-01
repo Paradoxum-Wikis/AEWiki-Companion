ee452ea20c333fb280a6ec6a676fceb3b8c14f3f
Benjamin <gabonnie1234@gmail.com>
2025-07-01 15:30:01 -0400

    feat: new main page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from "path";
 
 export default defineConfig({
   root: "src",
+  publicDir: resolve(__dirname, "public"),
   build: {
     outDir: "../dist",
     emptyOutDir: true,
